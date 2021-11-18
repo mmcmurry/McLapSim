fa748b91ad3c14bf0cfe81c6e0deb052cf701235
mmcmurry <matt@mattmcmurry.com>
2021-11-17 19:16:29 -0800

    Change aero functions to use coefficients

--- Car.py
+++ Car.py
@@ -1,41 +1,31 @@
 from utils import *
 
 class Car:
-    def __init__(self, name, m, P, mu, width, a_brake, df, drag):
-        self.name = name # Just the name of the instance
-        self.mass = m # Mass of the car with fuel and driver, in kilograms
-        self.power = P # Average power of the engine, calculated using acceleration event times, in Watts
-        self.mu = mu # Coefficient of static friction
-        self.a_brake = a_brake # Longitudinal acceleration under peak braking
-        self.df = df # A length 3 array with quadratic coefficients
-        self.df += findVertex(df[0],df[1],df[2])[::-1]
-        self.drag = drag # Same as self.df ... [a, b, c]
-        self.drag += findVertex(drag[0],drag[1],drag[2])[::-1]
-        self.width = width # Width of the car in meters
+    def __init__(self, name, m, P, mu, a_brake, cla, cda):
+        self.name = name        # Just the name of the instance
+        self.mass = m           # Mass of the car with fuel and driver, in kilograms
+        self.power = P          # Average power of the engine, calculated using acceleration event times, in Watts
+        self.mu = mu            # Tire coefficient of friction
+        self.a_brake = a_brake  # Average longitudinal acceleration when braking
+        self.cla = cla          # Coefficient of lift * area (negative for downforce)
+        self.cda = cda          # Coefficient of drag * area (positive)
         self.dt = 0.001        
     
-    def getDownforce(self, velocity):
-        # Returns downforce amount (positive) in Newtons given a velocity using a linear/quadratic model of downforce
-        # If v is higher than vertex of downforce parabola, use quadratic model of downforce. Otherwise use linear
-        if (velocity >= self.df[4]):
-            return self.df[0]*velocity**2 + self.df[1]*velocity + self.df[2]
-        # If v is lower than the apex, use the linear model
-        else:
-            return velocity*self.df[3]/self.df[4]
+    def getDownforce(self, v, D):
+        # Returns downforce (N), generally > 0
+        # Inputs: v = velocity (m/s); D = air density (kg/m^3)
+        return -0.5 * self.cla * D * v**2
 
-    def getDrag(self, velocity):
-        # Returns drag force (positive) in Newtons given a velocity using a linear/quadratic model of drag
-        # Basically identical to Car.getDownforce()
-        if (velocity >= self.drag[4]):
-            return self.drag[0]*velocity**2 - self.drag[1]*velocity + self.drag[2]
-        else:
-            return velocity*self.drag[3]/self.drag[4]
+    def getDrag(self, v, D):
+        # Returns drag (N), generally > 0
+        # Inputs: v = velocity (m/s); D = air density (kg/m^3)
+        return 0.5 * self.cda * D * v**2
     
     def findCorneringSpeed(self, radius):
         # Returns the maximum speed in meters per second that the car could go through a corner of given radius
         # Finds speed where centripetal force = cornering force. This formula can be reduced into a quadratic and solved
 
-        radius = radius + 0.5*self.width
+        radius = radius
         
         a = self.mass/radius - self.mu*self.df[0]
         b = self.mu*self.df[1]
@@ -55,7 +45,7 @@ class Car:
         # Returns time to go through a corner in seconds given the radius of the turn in meters and the angle of the turn
         # Uses Car.findCorneringSpeed() to find velocity through turn, then computes time through turn with t = d/v
         v = self.findCorneringSpeed(radius)
-        radius = radius + 0.5*self.width
+        radius = radius
         d = 2*pi*radius * theta/360
         return d/v
     
