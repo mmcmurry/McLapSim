c96aef71e41819b675b275b7845abb0924ce619e
mmcmurry <matt@mattmcmurry.com>
2021-11-18 18:54:23 -0800

    Add saturate function to utils

--- utils.py
+++ utils.py
@@ -8,3 +8,7 @@ def quadraticFormula(a,b,c):
     result.append((-b+sqrt(b**2-4*a*c))/(2*a))
     result.append((-b-sqrt(b**2-4*a*c))/(2*a))
     return result
+
+def saturate(x, lb, ub):
+    # Equivalent to Simulink saturation block. Bounds x between upper and lower bounds lb and ub.
+    return max(lb, min(ub, x))
