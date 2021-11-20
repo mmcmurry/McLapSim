d72fb9d4f3d478eac47e126a5c55a4d018df1806
mmcmurry <matt@mattmcmurry.com>
2021-11-19 20:23:18 -0800

    Rename Powertrain variables, refactor overrev & underrev

--- Powertrain.py
+++ Powertrain.py
@@ -1,40 +1,34 @@
+from math import pi
+
 class Powertrain:
-	def __init__(self, power_curve, gear_ratios, crank_sprocket_ratio, tire_radius):
-		self.power_curve = power_curve # A list of length 2 lists that are points on the power curve
-		self.min_rpm = power_curve[0][0]
-		self.max_rpm = power_curve[len(power_curve)-1][0]
-		self.min_power = power_curve[0][1]
-		self.max_power = power_curve[len(power_curve)-1][1]
-		self.gear_ratios = gear_ratios
-		self.crank_sprocket_ratio = crank_sprocket_ratio
-		self.tire_radius = tire_radius
+	def __init__(self, PEngineTable, rGearRatios, rFinalDriveRatio, rTire):
+		self.PEngineTable = PEngineTable # A list of length 2 lists that are points on the power curve
+		self.nEngineMin = PEngineTable[0][0]
+		self.nEngineMax = PEngineTable[len(PEngineTable)-1][0]
+		self.PEngineMin = PEngineTable[0][1]
+		self.PEngineMax = PEngineTable[len(PEngineTable)-1][1]
+		self.rGearRatios = rGearRatios
+		self.rFinalDriveRatio = rFinalDriveRatio
+		self.rTire = rTire
 		
-		for i in range(len(power_curve)-1):
-			rise = power_curve[i][1]-power_curve[i+1][1]
-			run = power_curve[i][0]-power_curve[i+1][0]
-			slope = rise/run
-			power_curve[i].append(slope) # The slope from point i to point i+1
+		for i in range(len(PEngineTable)-1):
+			dP_dn = (PEngineTable[i][1]-PEngineTable[i+1][1]) / (PEngineTable[i][0]-PEngineTable[i+1][0])
+			PEngineTable[i].append(dP_dn) # The slope from point i to point i+1
 		
-		self.rpm = self.min_rpm
+		self.rpm = self.nEngineMin
 		self.gear = 1
 	
 	def overrev(self):
-		if self.rpm > self.max_rpm:
-			return True
-		else:
-			return False
+		return self.rpm > self.nEngineMax
 	
 	def underrev(self):
-		if self.rpm < self.min_rpm:
-			return True
-		else:
-			return False
+		return self.rpm < self.nEngineMin
 	
 	def setRPM(self, velocity):
 	# Given velocity, find RPM based on gearing. Haven't been able to get this to put out realistic numbers
-		self.rpm = velocity/self.tire_radius * self.gear_ratios[self.gear-1] * self.crank_sprocket_ratio * 60 / (2*pi)
-		if self.rpm < self.min_rpm and self.gear == 1: # Don't let the engine 'stall'
-			self.rpm = self.min_rpm
+		self.rpm = velocity/self.rTire * self.rGearRatios[self.gear-1] * self.rFinalDriveRatio * 60 / (2*pi)
+		if self.rpm < self.nEngineMin and self.gear == 1: # Don't let the engine 'stall'
+			self.rpm = self.nEngineMin
 		
 		return self.rpm
 	
@@ -44,7 +38,7 @@ class Powertrain:
 		while(self.overrev() or self.underrev()):
 			# Upshift if revs are too high and not in final gear
 			if self.overrev():
-				if self.gear != len(self.gear_ratios):
+				if self.gear != len(self.rGearRatios):
 					self.gear += 1
 				else:
 					#print("Redline in 6th!")
@@ -62,13 +56,13 @@ class Powertrain:
 		return self.gear	
 	
 	def getPower(self):
-	# Returns power in Watts based on current rpm of the engine. Uses linear interpolation between points in power_curve
+	# Returns power in Watts based on current rpm of the engine. Uses linear interpolation between points in PEngineTable
 	#!!! Doesn't give any power if less than min rpm !!!
 	#y=m(x-x1)+y1
 		power = 0
-		for i in range(len(self.power_curve)-1):
-			if self.rpm >= self.power_curve[i][0] and self.rpm < self.power_curve[i+1][0]:
-				power = self.power_curve[i][2] * (self.rpm - self.power_curve[i][0]) + self.power_curve[i][1]
+		for i in range(len(self.PEngineTable)-1):
+			if self.rpm >= self.PEngineTable[i][0] and self.rpm < self.PEngineTable[i+1][0]:
+				power = self.PEngineTable[i][2] * (self.rpm - self.PEngineTable[i][0]) + self.PEngineTable[i][1]
 			
 		return power
 	
@@ -79,5 +73,5 @@ class Powertrain:
 	
 	def getWheelForce(self):
 	# Converts torque from the engine to force on the ground
-		force = self.getTorque() * self.gear_ratios[self.gear-1] * self.crank_sprocket_ratio / self.tire_radius
+		force = self.getTorque() * self.rGearRatios[self.gear-1] * self.rFinalDriveRatio / self.rTire
 		return force 
